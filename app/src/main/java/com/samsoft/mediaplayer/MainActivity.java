778ee06d354178ec99811135b461f9879bfb79f7
Satyam <satyamkhanna66@gmail.com>
2020-06-01 03:48:38 +0530

    "FC"

---
+++ app/src/main/java/com/samsoft/mediaplayer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.samsoft.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView)findViewById(R.id.videoplayer);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+
+        MediaController mediaController =  new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+
+    }
+}
